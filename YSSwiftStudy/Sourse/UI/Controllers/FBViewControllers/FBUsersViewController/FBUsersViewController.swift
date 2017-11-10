13ad3dc0a679e7ef6890177b5f525da084efb77e
YS <yuriisushko@MacBook-Pro-Yurii.local>
2017-11-10 17:57:14 +0200

    friendsViewController

--- YSSwiftStudy/Sourse/UI/Controllers/FBViewControllers/FBUsersViewController/FBUsersViewController.swift
+++ YSSwiftStudy/Sourse/UI/Controllers/FBViewControllers/FBUsersViewController/FBUsersViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class FBUsersViewController: FBViewController {
+class FBUsersViewController: FBViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Constants
+    
+    private struct Constants {
+        static let title = "Friends"
+    }
     
     // MARK: Public properties
     
     typealias ViewType = FBUserView
     var rootView: FBUsersView?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
     
+    var usersModel:UsersModel? {
+        return (self.model as! UsersModel)
+    }
+
     // MARK: Private properties
     
     private var logoutContext:FBLogoutContext? {
@@ -31,6 +36,42 @@ class FBUsersViewController: FBViewController {
             oldValue?.cancel()
         }
     }
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if  let model = self.model {
+             self.context = GetUsersContext(model: model)
+        }
+    }
+    
+    // MARK: IBAction
+    
+    @IBAction func logOut(sender: UIButton) {
+        self.logoutContext = FBLogoutContext()
+    }
+    
+    // MARK: Public methods
+    
+    override func fill(with model: Model) {
+        self.navigationItem.title = Constants.title
+        self.rootView?.tableView?.reloadData()
+    }
+    
+     // MARK: UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let model = self.usersModel {
+            return model.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
-
+    
 }
